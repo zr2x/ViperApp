ff607419c65a87f012fc3c96ba4aa7874693e561
Iskander Sitdikov <master5212345@gmail.com>
2023-11-12 13:06:53 +0500

    Initial commit

---
+++ ViperApp/Modules/Main/Interactor/MainInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  MainInteractor.swift
+//  ViperApp
+//
+//  Created by Искандер Ситдиков on 12.11.2023.
+//
+
+import Foundation
--- ViperApp/Modules/Main/View/MainTableViewCell.swift
+++ ViperApp/Modules/Main/View/MainTableViewCell.swift
@@ -10,5 +10,64 @@ import UIKit
 class MainTableViewCell: UITableViewCell {
 
     static let identifire = "MainTableViewCell"
+    
+    
+    //MARK: UI
+    private var avatarImageView: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleToFill
+        image.layer.cornerRadius = 10
+        
+        return image
+    }()
+    
+    private var nameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    private var statusLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        
+        return label
+    }()
 
+    private var favouriteButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 25
+
+        button.addTarget(nil, action: #selector(didTapButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        
+        setupConstraints()
+    }
+    
+    
+    private func setupConstraints() {
+        
+    }
+    
+    @objc
+    private func didTapButton() {
+        
+    }
+    
 }
